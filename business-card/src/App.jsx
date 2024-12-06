81d7a1bf2f6b31225907d60ea906ee07331fea94
Andrey Pisarets <pisarets.andrey@gmail.com>
2024-12-06 14:13:26 +0300

    Changed components' content

--- business-card/src/App.jsx
+++ business-card/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import githubLogo from '/github-mark.svg'
+import tgLogo from '/telegram_logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+                </div>
+                <div>
+                    <a href="https://github.com/psrtsandr" target="_blank">
+                        <img src={githubLogo} className="github-logo" alt="GitHub logo" />
+                    </a>
+                    <a href="https://t.me/psrtsandr" target="_blank">
+                        <img src={tgLogo} className="logo" alt="Telegram logo" />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
