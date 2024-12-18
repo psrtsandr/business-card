c9e64afc6c8d7b2005623cd7f6e993e8ed128bb1
Andrey Pisarets <pisarets.andrey@gmail.com>
2024-12-18 20:11:42 +0300

    Revert "Changed components' content"
    This reverts commit f9b9a5277fb94834e4cb6b682f658d73bfb6f1b2.

--- business-card/src/App.jsx
+++ business-card/src/App.jsx
@@ -1,26 +1,35 @@
-import githubLogo from '/github-mark.svg'
-import tgLogo from '/telegram_logo.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    return (
-        <>
-            <div>
-                <div>
-                    <p>Всем привет!</p>
-                    <span>Я являюсь разработчиком настольных и веб-приложений. Мой карьерный путь начался в 2022 году. Начинал с разработки программных модулей для программного комплекса, предназначенного для моделирования систем безопасности. Владею C#, WPF, Entity Framework, ASP.NET, MS SQL Server и другими технологиями.</span>
-                </div>
-                <div>
-                    <a href="https://github.com/psrtsandr" target="_blank">
-                        <img src={githubLogo} className="github-logo" alt="GitHub logo" />
-                    </a>
-                    <a href="https://t.me/psrtsandr" target="_blank">
-                        <img src={tgLogo} className="logo" alt="Telegram logo" />
-                    </a>
-                </div>
-            </div>
-        </>
-    )
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
